08df56984b7c331f28117524ad0bad19caf25ddd
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-01-20 01:16:50 +0600

    jwt token verification done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const morgan = require('morgan')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -36,9 +37,55 @@ async function run() {
     const userCollection = client.db("swiftParcelDB").collection("users");
     const parcelCollection = client.db("swiftParcelDB").collection("parcels");
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
+      res.send({ token });
+    })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.userType === 'Admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+    // use verify DeliveryMan after verifyToken
+    const verifyDeliveryMan = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.userType === 'DeliveryMan';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+
 
     // Payment
-    app.post("/create-payment-intent", async (req, res) => {
+    app.post("/create-payment-intent", verifyToken, async (req, res) => {
       const { price } = req.body;
       const paidAmount = parseInt(price * 100); // Amount should be in cents
 
@@ -54,7 +101,7 @@ async function run() {
     });
 
 
-    app.put("/user/updateParcelPayment/:id", async (req, res) => {
+    app.put("/user/updateParcelPayment/:id", verifyToken, async (req, res) => {
       const parcelId = req.params.id;
       const { paymentId, paidAmount, paymentStatus } = req.body;
       const query = { _id: new ObjectId(parcelId) };
@@ -77,13 +124,13 @@ async function run() {
     // user related apis
 
     // get all user
-    app.get("/all/users", async (req, res) => {
+    app.get("/all/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
 
-    // get all user except current user and delivery man
-    app.get("/all/users/:email", async (req, res) => {
+    // get all user  and delivery man except current user
+    app.get("/all/users/:email", verifyToken, verifyAdmin, async (req, res) => {
       const email = req.params.email;
 
       // Query to exclude the current user and only include users with userType "User"
@@ -113,7 +160,7 @@ async function run() {
     })
 
     // get a specific user data
-    app.get("/user/role/:email", async (req, res) => {
+    app.get("/user/role/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email }
       const result = await userCollection.findOne(query);
@@ -121,13 +168,13 @@ async function run() {
     })
 
     // Get statistics data
-    app.get("/parcels/statistics", async (req, res) => {
+    app.get("/parcels/statistics", verifyToken, verifyAdmin, async (req, res) => {
       const parcels = await parcelCollection.find({}).toArray();
       res.send(parcels);
     });
 
     // Update user role
-    app.patch("/user/update/role/:id", async (req, res) => {
+    app.patch("/user/update/role/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const { userType } = req.body;
       const query = { _id: new ObjectId(id) };
@@ -145,7 +192,7 @@ async function run() {
     // })
 
 
-    app.get("/all/users-with-parcels/:email", async (req, res) => {
+    app.get("/all/users-with-parcels/:email", verifyToken, verifyAdmin, async (req, res) => {
       const email = req.params.email;
 
       // Query to exclude the current user and only include userType "User"
@@ -191,6 +238,7 @@ async function run() {
     // Update a specific user
     app.patch(
       '/user/update/profile/:email',
+      verifyToken,
       async (req, res) => {
         const email = req.params.email;
         const { name, photoURL } = req.body;
@@ -217,14 +265,14 @@ async function run() {
     )
 
     // get all delivery man
-    app.get("/all/deliveryMan", async (req, res) => {
+    app.get("/all/deliveryMan", verifyToken, verifyAdmin, async (req, res) => {
       const query = { userType: "DeliveryMan" };
       const result = await userCollection.find(query).toArray();
       res.send(result);
     })
 
-
-    app.get("/reviews/:deliveryManId", async (req, res) => {
+    // get reviews of a delivery man
+    app.get("/reviews/:deliveryManId", verifyToken, verifyDeliveryMan, async (req, res) => {
       const { deliveryManId } = req.params;
       const reviews = await parcelCollection.find({ deliveryManId }).toArray();
       res.send(reviews);
@@ -232,15 +280,15 @@ async function run() {
 
 
     // get add delivery request of a delivery man
-    app.get("/all/deliveryRequests/:id", async (req, res) => {
+    app.get("/all/deliveryRequests/:id", verifyToken, verifyDeliveryMan, async (req, res) => {
       const id = req.params.id;
       const query = { deliveryManId: id, status: "on the way" };
       const result = await parcelCollection.find(query).toArray();
       res.send(result);
     })
 
-    // add review and feedback by user
-    app.put("/user/submitReview/:id", async (req, res) => {
+    // add review and feedback by a normal user
+    app.put("/user/submitReview/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const { rating, feedback, reviewDate } = req.body;
 
@@ -263,12 +311,12 @@ async function run() {
     // parcel related apis
 
     // get all parcels
-    app.get("/all/parcels", async (req, res) => {
+    app.get("/all/parcels", verifyToken, verifyAdmin, async (req, res) => {
       const result = await parcelCollection.find().toArray();
       res.send(result);
     })
-    // get a parcel data based on id
-    app.get("/parcel/:id", async (req, res) => {
+    // get a parcel data based on id by user
+    app.get("/user/parcel/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await parcelCollection.findOne(query);
@@ -276,7 +324,7 @@ async function run() {
     })
 
     // get all parcels of a specific user
-    app.get("/all/parcels/:email", async (req, res) => {
+    app.get("/all/parcels/:email", verifyToken, async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const result = await parcelCollection.find(query).toArray();
@@ -284,7 +332,7 @@ async function run() {
     })
 
     // search parcels on requestedDeliveryDate
-    app.get("/parcels/search", async (req, res) => {
+    app.get("/parcels/search", verifyToken, verifyAdmin, async (req, res) => {
       const { from, to } = req.query;
       console.log(from, to);
       const query = {
@@ -299,7 +347,7 @@ async function run() {
 
 
 
-    app.post("/book/parcel", async (req, res) => {
+    app.post("/book/parcel", verifyToken, async (req, res) => {
       const parcel = req.body;
       const result = await parcelCollection.insertOne({ ...parcel, bookingDate: new Date() });
       res.send(result);
@@ -310,6 +358,8 @@ async function run() {
     // Update a selected parcel status and add deliveryManId, approximateDate
     app.put(
       '/admin/update/parcel/:id',
+      verifyToken,
+      verifyAdmin,
       async (req, res) => {
         const id = req.params.id;
         const { selectedDeliveryManId, approximateDate } = req.body;
@@ -328,7 +378,7 @@ async function run() {
 
 
     // update a parcel status by a delivery man
-    app.put("/deliveryMan/update/parcel/:id", async (req, res) => {
+    app.put("/deliveryMan/update/parcel/:id", verifyToken, verifyDeliveryMan, async (req, res) => {
       const id = req.params.id;
       const { status } = req.body;
       const query = { _id: new ObjectId(id) };
@@ -344,6 +394,7 @@ async function run() {
     // Update a specific parcel data by user
     app.patch(
       '/user/update/parcel/:id',
+      verifyToken,
       async (req, res) => {
         const id = req.params.id;
         const { name,
@@ -381,7 +432,7 @@ async function run() {
     )
 
     // Cancel a specific parcel order by user
-    app.patch("/user/cancel/parcel/:id", async (req, res) => {
+    app.patch("/user/cancel/parcel/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const { status } = req.body;
       const query = { _id: new ObjectId(id) }
